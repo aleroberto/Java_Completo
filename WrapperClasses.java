452ddd94cedae739f1adb2fbd255cac9c64940e1
Ale Roberto <alexandreroberto2@gmail.com>
2020-06-28 17:20:32 -0300

    Exclusão de 'outros'
    Exclusão de 'outros' tipos que não correspondem ao contexto do tema abordado.

--- WrapperClasses.java
+++ WrapperClasses.java
@@ -1,15 +1,6 @@
 import java.lang.Math;
 
-class TiposPrimitivos {
-    byte pByte;
-    short pShort;
-    int pInt;
-    long pLong;
-    float pFloat;
-    double pDouble;
-    char pChar;
-    boolean pBoolean;
-
+class WrapperClasses {
     Byte wByte;
     Short wShort;
     Integer wInteger;
@@ -19,32 +10,8 @@ class TiposPrimitivos {
     Boolean wBoolean;
 
     public static void main(String[] args) {
-        TiposPrimitivos tiposPrimitivos = new TiposPrimitivos();
-        tiposPrimitivos.pBoolean = true;
-        
-        //minimo e maximo tipos primitivos
-        tiposPrimitivos.pByte = 127;
-        tiposPrimitivos.pByte = -127;
-
-        tiposPrimitivos.pShort = -32768;
-        tiposPrimitivos.pShort = 32767;
+        WrapperClasses WrapperClasses = new WrapperClasses();
        
-        tiposPrimitivos.pInt = (int)(Math.pow(2, 31) - 1); //utilizado precedencia para evitar truncate
-        tiposPrimitivos.pInt = (int) Math.pow(-2, 31);
-
-        tiposPrimitivos.pLong = (long)(Math.pow(2, 63) - 1);
-        tiposPrimitivos.pLong = (long) Math.pow(2, 63);
-
-        //float armazena até 32 bits
-        tiposPrimitivos.pFloat = Float.MAX_VALUE;
-        tiposPrimitivos.pFloat = Float.MIN_VALUE;
-
-        //double tem a maior precisão em relação a float
-        tiposPrimitivos.pDouble = Double.MAX_VALUE;
-        tiposPrimitivos.pDouble = Double.MIN_VALUE;
-
-        //CHAR aemazena um caracter utilizando 16 bits
-        tiposPrimitivos.pChar = 'J';
 
         //		System.out.println();
 
