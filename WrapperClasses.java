72fe06a3d1fd5852c1fd98506cbdd72c1db9564c
Ale Roberto <alexandreroberto2@gmail.com>
2020-06-28 15:04:26 -0300

    Create WrapperClasses.java
    initial commit

---
+++ WrapperClasses.java
@@ -0,0 +1,25 @@
+
+class WrapperClasses{
+	byte pByte;
+	short pShort;
+	int pInt;
+	long pLong;
+	float pFloat;
+	double pDouble;
+	char pChar;
+	boolean pBoolean;
+
+	Byte wByte;
+	Short wShort;
+	Integer wInteger;
+	Long wLong;
+	Double wDouble;
+	Character wChar;
+	Boolean wBoolean;
+
+	public static void main(String[] args) {
+		System.out.println("comecamos");
+		
+	}
+
+}
