bf75f6e9c6a4f1f462dc9ba3d5252b02db42a851
Ale Roberto <alexandreroberto2@gmail.com>
2020-06-28 16:23:47 -0300

    tipos primitivos
    Criação paa definição de Wrapper's

--- WrapperClasses.java
+++ WrapperClasses.java
@@ -1,3 +1,5 @@
+import java.lang.Math;
+
 
 class WrapperClasses{
 	byte pByte;
@@ -18,7 +20,25 @@ class WrapperClasses{
 	Boolean wBoolean;
 
 	public static void main(String[] args) {
-		System.out.println("comecamos");
+		WrapperClasses wrapper = new WrapperClasses();
+		wrapper.pBoolean = true;
+			//minimo e maximo tipos primitivos
+		
+		wrapper.pByte = 127;
+		wrapper.pByte = -127;
+
+		wrapper.pShort = -32768;
+		wrapper.pShort = 32767;
+
+			//utilizado precedencia para evitar truncate
+		wrapper.pInt = (int)(Math.pow(2, 31) - 1);
+		wrapper.pInt = (int) Math.pow(-2 ,31);
+
+		wrapper.pLong = (long)(Math.pow(2,63)-1);
+		wrapper.pLong = (long) Math.pow(2,63);
+
+
+		//System.out.println(wrapper.pInt);
 		
 	}
 
